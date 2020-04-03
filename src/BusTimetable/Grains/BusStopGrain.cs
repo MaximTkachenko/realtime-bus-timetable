9e839cf187544a77f04ec868b39455c5d2a10ad0
Maxim Tkachenko <oblomov86@gmail.com>
2020-04-03 09:15:00 +0200

    Update BusStopGrain.cs

--- src/BusTimetable/Grains/BusStopGrain.cs
+++ src/BusTimetable/Grains/BusStopGrain.cs
@@ -31,6 +31,7 @@ namespace BusTimetable.Grains
         //todo this method should be optimized using:
         // - https://dotnet.github.io/orleans/Documentation/grains/stateless_worker_grains.html
         // - https://github.com/dotnet/orleans/issues/5283#issuecomment-450977696- 
+        // - https://github.com/dotnet/orleans/issues/3841
         // - https://stackoverflow.com/questions/48145496/msr-orleans-how-to-create-a-reader-writer-grain-with-parallel-reads/48146722#48146722
         public Task<IReadOnlyList<TimeTableItem>> GetTimeTable()
         {
