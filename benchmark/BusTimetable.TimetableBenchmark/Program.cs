2872e574cecd54f901c60faabc2da1ea29eb1c6b
Tkachenko <Maxim.Tkachenko@mhwirth.com>
2020-07-14 08:00:55 +0200

    moved benchmark project to separate folder

---
+++ benchmark/BusTimetable.TimetableBenchmark/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace BusTimetable.TimetableBenchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<TimetableTest>();
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class TimetableTest
+    {
+        //todo add benchmarks
+    }
+}
