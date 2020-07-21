a7939d77ab2a125a866a10fd90a7f8dcf8b7c2ce
mtkachenko <oblomov86@gmail.com>
2020-07-21 21:34:39 +0200

    added toggle to switch from reading from grain to reading from cache

--- client/src/Timetable.tsx
+++ client/src/Timetable.tsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import { Root } from './Metadata';
+import * as rb from 'react-bootstrap';
 
 type TimetableProps = { server: string, metadata: Root };
-type TimetableState = { busStopId: string, items: TimetableItem[], intervalId: NodeJS.Timeout | null, error: boolean };
+type TimetableState = { busStopId: string, items: TimetableItem[], intervalId: NodeJS.Timeout | null, error: boolean, getMode: number };
 type TimetableItem = { routeId: string, msBeforeArrival: number, direction: number };
 
 export default class Timetable extends Component<TimetableProps, TimetableState> {
     constructor(props: TimetableProps){
       super(props);
-      this.state = {busStopId: '', items: [], intervalId: null, error: false};
+      this.state = {busStopId: '', items: [], intervalId: null, error: false, getMode: 1};
+
+      this.selectBusStop = this.selectBusStop.bind(this);
+      this.setGetMode = this.setGetMode.bind(this);
     }
 
     componentDidMount() {
@@ -22,7 +26,11 @@ export default class Timetable extends Component<TimetableProps, TimetableState>
         }
 
         const intervalId = setInterval(async () => {
-            fetch(this.props.server + '/' + busStopId + '/timetable')
+            const getUrl = this.state.getMode === 1
+                ? this.props.server + '/' + busStopId + '/timetable'
+                : this.props.server + '/' + busStopId + '/cache/timetable';
+
+            fetch(getUrl)
                 .then(response => response.json())
                 .then(response => this.setState({ items: response, busStopId: busStopId, error: false }))
             .catch(error => this.setState({items: [], error: true}));
@@ -31,23 +39,34 @@ export default class Timetable extends Component<TimetableProps, TimetableState>
         this.setState({intervalId: intervalId});
     }
 
-    render(){
+    setGetMode(getMode: number){
+        this.setState({getMode: getMode});   
+    }
+
+    render(){        
+        const getModeSelector = (<rb.ToggleButtonGroup type="radio" name="options" defaultValue={1} onChange={this.setGetMode}>
+            <rb.ToggleButton value={1}>From grain</rb.ToggleButton>
+            <rb.ToggleButton value={2}>From cache</rb.ToggleButton>
+        </rb.ToggleButtonGroup>);
+        
         if(this.state.error){
             return (
                 <div>
+                    {getModeSelector}
                     <h4>{this.state.busStopId}</h4>
-                    <div key='error' className='alert alert-danger alert-link' role="alert">Unable to connect to server.</div>
+                    <rb.Alert key='error' variant='danger'>Unable to connect to server.</rb.Alert>
                 </div>
             );
         }
         
         const listItems = this.state.items.map((item) =>            
-            <div key={item.routeId} className='alert alert-primary alert-link' role="alert">
+            <rb.Alert key={item.routeId} variant='primary'>
                 [{item.direction === 1 ? "THERE" : "BACK"}] {item.routeId}: {item.msBeforeArrival <= this.props.metadata.nowThresholdSec ? 'now' : `${item.msBeforeArrival}sec`}
-            </div>
+            </rb.Alert>
         );
         return (
             <div>
+                {getModeSelector}
                 <h4>{this.state.busStopId}</h4>
                 {listItems}
             </div>
